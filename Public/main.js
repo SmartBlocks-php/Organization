3a47704ce43915cd73501bef3d22981840b0daf2
Antoine Jackson <a.j.william26@gmail.com>
2013-10-01 19:14:04 +0200

    + code for notifications

--- Public/main.js
+++ Public/main.js
@@ -7,6 +7,40 @@ define([
     var main = {
         init: function () {
             var base = this;
+            if (SmartBlocks.Blocks.Notifications) {
+                setInterval(function () {
+                    base.checkReviewTime();
+                }, 1000);
+            }
+        },
+        checkReviewTime: function () {
+            var base = this;
+            console.log("checked for daily review");
+            var now = new Date();
+            var day = now.getDate();
+            var month = now.getMonth() + 1;
+            var year = now.getFullYear();
+            if (now.getHours() > 16) {
+                var identifier = day + "-" + month + "-" + year;
+                var notification = SmartBlocks.Blocks.Notifications.Data.notifications.find(function (notification) {
+                    return notification.get("organization_data") && notification.get("organization_data").id == identifier;
+                });
+                if (notification) {
+                    SmartBlocks.Blocks.Notifications.Methods.notify(notification);
+                } else {
+                    var notification = SmartBlocks.Blocks.Notifications.Methods.createNotification({
+                        title: "Daily review",
+                        type: "information",
+                        content: "It is time for you to review your day's tasks and to plan tomorrow.",
+                        destination: "#Organization/desk/review",
+                        sticky: true,
+                        organization_data: {
+                            id: identifier,
+                            done: false
+                        }
+                    });
+                }
+            }
         },
         organization: function () {
             var base = this;
