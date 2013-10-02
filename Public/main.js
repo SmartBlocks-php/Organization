97df5741c996de74ab31f2faef3c7409fa444cc7
Antoine Jackson <a.j.william26@gmail.com>
2013-10-02 16:32:55 +0200

    + shortcut for notification tests

--- Public/main.js
+++ Public/main.js
@@ -12,34 +12,45 @@ define([
                     base.checkReviewTime();
                 }, 1000);
             }
+
+            SmartBlocks.Shortcuts.add([
+                101
+            ], function () {
+                base.notifyReviewTime();
+            });
         },
         checkReviewTime: function () {
             var base = this;
             console.log("checked for daily review");
             var now = new Date();
+            if (now.getHours() > 16) {
+                base.notifyReviewTime();
+            }
+        },
+        notifyReviewTime: function () {
+            var base = this;
+            var now = new Date();
             var day = now.getDate();
             var month = now.getMonth() + 1;
             var year = now.getFullYear();
-            if (now.getHours() > 16) {
-                var identifier = day + "-" + month + "-" + year;
-                var notification = SmartBlocks.Blocks.Notifications.Data.notifications.find(function (notification) {
-                    return notification.get("organization_data") && notification.get("organization_data").id == identifier;
+            var identifier = day + "-" + month + "-" + year;
+            var notification = SmartBlocks.Blocks.Notifications.Data.notifications.find(function (notification) {
+                return notification.get("organization_data") && notification.get("organization_data").id == identifier;
+            });
+            if (notification) {
+                SmartBlocks.Blocks.Notifications.Methods.notify(notification);
+            } else {
+                var notification = SmartBlocks.Blocks.Notifications.Methods.createNotification({
+                    title: "Daily review",
+                    type: "information",
+                    content: "It is time for you to review your day's tasks and to plan tomorrow.",
+                    destination: "#Organization/desk/review",
+                    sticky: true,
+                    organization_data: {
+                        id: identifier,
+                        done: false
+                    }
                 });
-                if (notification) {
-                    SmartBlocks.Blocks.Notifications.Methods.notify(notification);
-                } else {
-                    var notification = SmartBlocks.Blocks.Notifications.Methods.createNotification({
-                        title: "Daily review",
-                        type: "information",
-                        content: "It is time for you to review your day's tasks and to plan tomorrow.",
-                        destination: "#Organization/desk/review",
-                        sticky: true,
-                        organization_data: {
-                            id: identifier,
-                            done: false
-                        }
-                    });
-                }
             }
         },
         organization: function () {
